f8027ab4e572bb47cd4884b9dc48d229865c520c
Juan Huttemann <juanfhuttemann@gmail.com>
2019-09-30 15:31:10 -0400

    add navigation

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,10 +1,11 @@
 import React, {Fragment} from 'react'
-import classes from './Layout.css'
+import './Layout.css'
+import Toolbar from '../Navigation/Toolbar/Toolbar'
 
 const layout = (props) => (
     <Fragment>
-        <div>Toolbar, Navbar</div>
-        <main className={classes.Content}>
+        <Toolbar/>
+        <main className="Content">
             {props.children}
         </main>
     </Fragment>
